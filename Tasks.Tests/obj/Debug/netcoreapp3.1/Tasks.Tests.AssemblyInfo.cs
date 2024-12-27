f0fa4d8aaf217f7fa97a270a17ba9d00bc08853b
srdrflk <55311271+srdrflk@users.noreply.github.com>
2024-12-27 17:13:21 +0300

    Part1 completed

--- Tasks.Tests/obj/Debug/netcoreapp3.1/Tasks.Tests.AssemblyInfo.cs
+++ Tasks.Tests/obj/Debug/netcoreapp3.1/Tasks.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasks.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+506c66098cc0fed49a5f7b8b4cb30e79a38bd7a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasks.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasks.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tasks/DoublyLinkedList.cs
+++ Tasks/DoublyLinkedList.cs
@@ -1,47 +1,98 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 using Tasks.DoNotChange;
 
 namespace Tasks
 {
     public class DoublyLinkedList<T> : IDoublyLinkedList<T>
     {
-        public int Length => throw new NotImplementedException();
-
+        public int Length { get; set; }
+        //public int Length => throw new NotImplementedException();
+        LinkedList<T> linkedList = new LinkedList<T>();
         public void Add(T e)
         {
-            throw new NotImplementedException();
+            linkedList.AddLast(e);
+            Length = linkedList.Count;
         }
 
         public void AddAt(int index, T e)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > linkedList.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var currentNode = linkedList.First;
+            for (int i = 0; i < index && currentNode != null; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+            if (currentNode == null)
+            {
+                linkedList.AddLast(e);
+                Length = linkedList.Count;
+            }
+            else
+            {
+                linkedList.AddBefore(currentNode, e);
+                Length = linkedList.Count;
+            }
         }
 
         public T ElementAt(int index)
         {
-            throw new NotImplementedException();
+            if (linkedList.Count == 0 || linkedList.Count <= index || index < 0) throw new IndexOutOfRangeException();
+
+            return linkedList.ElementAt(index);
+
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return linkedList.GetEnumerator();
         }
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            if (linkedList.Count == 0) throw new IndexOutOfRangeException();
+
+            Length = linkedList.Count;
+
+            if (linkedList.Contains(item))
+            {
+                linkedList.Remove(item);
+                Length = linkedList.Count;
+            }
+            
         }
 
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (linkedList.Count == 0 || index < 0 || index >= linkedList.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var currentNode = linkedList.First;
+            for (int i = 0; i < index && currentNode != null; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            T removedValue = currentNode.Value;
+            linkedList.Remove(currentNode);
+            Length = linkedList.Count;
+
+            return removedValue;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return linkedList.GetEnumerator();
         }
     }
 }
